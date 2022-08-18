9fda94fe8a9bc29f2b69b7ac654c9701857d33a3
Satyajit Kuila <sanu123j@gmail.com>
2022-08-18 16:55:46 +0530

    numbers

---
+++ SeleniumCsharpTraining/CSharpPrograms/Day2/NumberDivisibility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCsharpTraining.CSharpPrograms.Day2
+{
+    internal class NumberDivisibility
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            int n = int.Parse(Console.ReadLine());
+
+            int k =n % 2;
+            int j=n % 3;
+
+            if(k == 0)
+            {
+                if (j != 0)
+                {
+                    Console.WriteLine($"{n} is divisible by 2");
+                }
+                else
+                {
+                    Console.WriteLine($"{n} is divisible by 2 and 3");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"{n} is not divisible by 2 ");
+            }
+
+        }
+    }
+}
