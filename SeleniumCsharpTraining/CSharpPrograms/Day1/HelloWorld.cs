3e8df8cb9f1a5d1296d060b6555e16badda11a0a
satyajit.kuila <satyajit.kuila@INBL01LAP8705>
2022-08-18 10:21:35 +0530

    Add project files.

---
+++ SeleniumCsharpTraining/CSharpPrograms/Day1/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCsharpTraining.CSharpPrograms.Day1
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Hello World! program");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SeleniumCsharpTraining/CSharpPrograms/Day1/Palindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCsharpTraining.CSharpPrograms.Day1
+{
+    internal class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Word:");
+            String str = Console.ReadLine();
+            int length = str.Length;
+            System.Console.WriteLine(length);
+            String reverse = String.Empty;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+            Console.WriteLine(reverse);
+            if (str.Equals(reverse))
+            {
+                Console.WriteLine($"{str} is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{str} is  not a palindrome");
+            }
+        }
+    }
+}
