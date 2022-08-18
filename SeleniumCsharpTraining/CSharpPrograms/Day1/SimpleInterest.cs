05a7b5ec3ebaf451a27cc70542fba3d71cbd96da
Satyajit Kuila <satyajit.kuila@collabera.com>
2022-08-18 13:19:57 +0530

    SI program+

---
+++ SeleniumCsharpTraining/CSharpPrograms/Day1/SimpleInterest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCsharpTraining.CSharpPrograms.Day1
+{
+    internal class SimpleInterest
+    {
+        static void Main(string[] args)
+        {
+            double si = 0.0;
+            double amt = 0.0;
+            double p=0.0;  
+            double r=0.0;
+            double t=0.0;
+            
+            Console.WriteLine("Enter the Loan amount:");
+            p=double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Rate of interest:");
+            r = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the period of loan:");
+            t = double.Parse(Console.ReadLine());
+
+            si = (p * r * t) / 100;
+            amt = si + p;
+
+            Console.WriteLine($"The final amont to be paid to bank is {amt} for {t} years");
+        }
+    }
+}
