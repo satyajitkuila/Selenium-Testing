d3bc36414ea92811a69c662d3385573d81712c37
Satyajit Kuila <sanu123j@gmail.com>
2022-08-18 18:48:23 +0530

    day2 done

---
+++ SeleniumCsharpTraining/CSharpPrograms/Day2/FindArea.cs
@@ -0,0 +1,50 @@
+﻿namespace SeleniumCsharpTraining.CSharpPrograms.Day2
+{
+    internal class FindArea
+    {
+        static void Main(string[] args)
+        {
+            double l, b, h = 0;
+
+            double area;
+            Console.WriteLine("1.Square");
+            Console.WriteLine("2.Rectangele");
+            Console.WriteLine("3.Triangle");
+            Console.WriteLine("Enter your Choice to find area:");
+            int chc = int.Parse(Console.ReadLine());
+
+
+            if (chc == 1)
+            {
+                Console.WriteLine("enter length");
+                l = double.Parse(Console.ReadLine());
+                area = l * l;
+                Console.WriteLine($"The area is {area}");
+            }
+            else if (chc == 2)
+            {
+                Console.WriteLine("enter length");
+                l = double.Parse(Console.ReadLine());
+                Console.WriteLine("enter breadth");
+                b = double.Parse(Console.ReadLine());
+                area = l * b;
+                Console.WriteLine($"The area is {area}");
+            }
+            else if (chc == 3)
+            {
+                Console.WriteLine("enter breadth");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine("enter height");
+                h = double.Parse(Console.ReadLine());
+                area = (b * h) / 2;
+                Console.WriteLine($"The area is {area}");
+            }
+            else
+            {
+                Console.WriteLine($"{chc} is a invalid choice Try Again !");
+            }
+
+
+        }
+    }
+}
---
+++ SeleniumCsharpTraining/CSharpPrograms/Day2/OddEven.cs
@@ -0,0 +1,37 @@
+﻿namespace SeleniumCsharpTraining.CSharpPrograms.Day2
+{
+    internal class OddEven
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            Console.WriteLine("Enter the integer to check:");
+
+            while (true)
+            {
+
+                int n = int.Parse(Console.ReadLine());
+                if (n > 0)
+                {
+                    k = n % 2;
+                    if (k == 0)
+                    {
+                        Console.WriteLine($"{n} is an odd number");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{n} is an even number");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("try again with a valid input !");
+                }
+
+
+            }
+        }
+    }
+}
