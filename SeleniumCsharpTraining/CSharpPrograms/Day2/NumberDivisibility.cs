2518d3b3dc04c12aeb2c5cfcf7cfd69ab57c0326
Satyajit Kuila <sanu123j@gmail.com>
2022-08-18 17:08:33 +0530

    numbers+

--- SeleniumCsharpTraining/CSharpPrograms/Day2/NumberDivisibility.cs
+++ SeleniumCsharpTraining/CSharpPrograms/Day2/NumberDivisibility.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SeleniumCsharpTraining.CSharpPrograms.Day2
+﻿namespace SeleniumCsharpTraining.CSharpPrograms.Day2
 {
     internal class NumberDivisibility
     {
@@ -13,10 +7,10 @@ namespace SeleniumCsharpTraining.CSharpPrograms.Day2
             Console.WriteLine("Enter the Number");
             int n = int.Parse(Console.ReadLine());
 
-            int k =n % 2;
-            int j=n % 3;
+            int k = n % 2;
+            int j = n % 3;
 
-            if(k == 0)
+            if (k == 0)
             {
                 if (j != 0)
                 {
