a6f1d033c5da8afa0b10c457b735654507feabfc
Satyajit Kuila <satyajit.kuila@collabera.com>
2022-08-18 11:23:06 +0530

    Area of circle done

--- SeleniumCsharpTraining/CSharpPrograms/Day1/AreaOfCircle.cs
+++ SeleniumCsharpTraining/CSharpPrograms/Day1/AreaOfCircle.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace SeleniumCsharpTraining.CSharpPrograms.Day1
 {
-    internal class AreaOfCircle
+     class AreaOfCircle
     {
+        static void Main(String[] args)
+        {
+            int rad = 0;
+            double area = 0.0;
+            Console.WriteLine("Enter the radius");
+            String r= Console.ReadLine();
+            rad = Convert.ToInt32(r);
+            area =3.141*rad*rad;
+            Console.WriteLine($"Area of circle is {area}");
+
+
+        }
     }
 }
