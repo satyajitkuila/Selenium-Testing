b5a67acd9fac1c907efb71fd643e574772daac42
Satyajit Kuila <sanu123j@gmail.com>
2022-08-20 18:21:20 +0530

    day4-01

---
+++ SeleniumCsharpTraining/CSharpPrograms/Day4/DictionaryDemo.cs
@@ -0,0 +1,33 @@
+﻿namespace SeleniumCsharpTraining.CSharpPrograms.Day4
+{
+    internal class DictionaryDemo
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> myDict = new Dictionary<int, string>();
+            myDict.Add(1, "Tom:CEO:54");
+            myDict.Add(2, "Rads:HR Head:65");
+            myDict.Add(3, "Fiona:CTO:43");
+            myDict.Add(4, "Daniel:CFO:59");
+
+            Console.WriteLine(myDict[3]);
+
+            for (int i = 1; i <= myDict.Count; i++)
+            {
+                String emp = myDict[i];
+                String[] arrOfStr = emp.Split(':');
+                Console.WriteLine($"Employee id: {i}");
+                for (int j = 0; j < arrOfStr.Length; j++)
+                {
+
+                    Console.WriteLine($"Name: {arrOfStr[j++]}");
+                    Console.WriteLine($"Designation: {arrOfStr[j++]}");
+                    Console.WriteLine($"age: {arrOfStr[j]}");
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ SeleniumCsharpTraining/CSharpPrograms/Day4/RegularExpression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SeleniumCsharpTraining.CSharpPrograms.Day4
+{
+    internal class RegularExpression
+    {
+        static void Main(string[] args)
+        {
+            //Q1.caat,cat,caaaat,caaaaaaaaat...## pattern = "^ca*t$";
+            //Q2.gold,told,bold,sold... ## pattern ="^[a-z]old$"
+            //Q3.mobile no of 10 digits starts with 98 ## pattern = @"^98\d{8}";
+           //Q4.DEF432G,GTH987M,VCD923$
+
+            RegularExpression rg =new RegularExpression();
+            String pattern = @"^[a-z]\d{3}[0-9]\d{3}[a-z]$";
+
+            bool val = rg.Equals(pattern);
+
+            
+            MatchCollection mt =Regex.Matches("DEF432G", pattern);
+            Console.WriteLine("{0} matches", mt.Count);
+
+        }
+    }
+}
