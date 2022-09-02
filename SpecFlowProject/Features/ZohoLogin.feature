45dba97bed629480f21ca22364e1de4742ffcad1
Satyajit Kuila <sanu123j@gmail.com>
2022-09-02 11:55:22 +0530

    Day15-001

---
+++ SpecFlowProject/Features/ZohoLogin.feature
@@ -0,0 +1,14 @@
+﻿Feature: ZohoLogin
+
+  Scenario Outline: Login to ZOHO
+    Given Zoho user on login page
+    When the user on zoho enters <username> and <password>
+    And he clicks on login button on zoho
+    When Zoho user is navigated to the home page
+    Then Close the browser on ZOHO
+
+    Examples: 
+      | username                   | password |
+      | raghav12                   |   123456 |
+      | ekhfejdj                   |  1234567 |
+      | samuelwitwicky56@gmail.com | kanhu143 |
---
+++ SpecFlowProject/StepDefinitions/ZohoLoginStepDefinitions.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SpecFlowProject.Support;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.StepDefinitions
+{
+    [Binding]
+    public class ZohoLoginStepDefinitions:DriverHelper
+
+    {
+        [Given(@"Zoho user on login page")]
+        public void GivenZohoUserOnLoginPage()
+        {
+            Driver.Url = "https://www.zoho.com/";
+            Driver.Manage().Window.Maximize();
+
+        }
+
+        [When(@"the user on zoho enters (.*) and (.*)")]
+        public void WhenTheUserOnZohoEntersRaghavAnd(string p0, string p1)
+        {
+            Driver.FindElement(By.XPath("//a[normalize-space()='Sign in']")).Click();
+            Driver.FindElement(By.XPath("//input[@id='login_id']")).SendKeys(p0);
+            Driver.FindElement(By.XPath("//form[@id='login']//button[@id='nextbtn']")).Click();
+
+            Thread.Sleep(2000);
+            Driver.FindElement(By.XPath("//input[@id='password']")).SendKeys(p1);
+        }
+
+        [When(@"he clicks on login button on zoho")]
+        public void WhenHeClicksOnLoginButtonOnZoho()
+        {
+            Driver.FindElement(By.XPath("//form[@id='login']//button[@id='nextbtn']")).Click();
+        }
+
+        [When(@"Zoho user is navigated to the home page")]
+        public void WhenZohoUserIsNavigatedToTheHomePage()
+        {
+            String title = Driver.Title;
+            Assert.AreEqual( "Zoho Accounts1",title, "User not on Home Page");
+
+        }
+
+        [Then(@"Close the browser on ZOHO")]
+        public void ThenCloseTheBrowserOnZOHO()
+        {
+            Thread.Sleep(2000);
+            Driver.FindElement(By.XPath("//img[@id='ztb-profile-image-pre']")).Click();
+            Thread.Sleep(2000);
+            Driver.FindElement(By.XPath("//div[@id='ztb-profile']")).Click();
+        }
+    }
+}
